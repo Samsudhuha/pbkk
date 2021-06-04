b6539c58643bd35199137bccacabcff36c9b008f
Samsudhuha <msamsudhuha9@gmail.com>
2021-06-04 10:48:06 +0800

    first commit

---
+++ application/views/personal/index.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Portfolio</title>
+	<meta name="description" content="Blvck - Personal vCard & Resume Template">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url('public/assets/images/favicon.png'); ?>">
+
+	<!-- STYLESHEETS -->
+	<link rel="stylesheet" href="<?php echo base_url('public/assets/bootstrap/css/bootstrap.min.css'); ?>" type="text/css" media="all">
+	<link rel="stylesheet" href="<?php echo base_url('public/assets/css/owl.carousel.css'); ?>" type="text/css" media="all">
+	<link rel="stylesheet" href="<?php echo base_url('public/assets/css/owl.theme.css'); ?>" type="text/css" media="all">
+	<link rel="stylesheet" href="<?php echo base_url('public/assets/css/font-awesome.min.css'); ?>" type="text/css" media="all">
+	<link rel="stylesheet" href="<?php echo base_url('public/assets/css/magnific-popup.css'); ?>" type="text/css" media="all">
+	<link rel="stylesheet" href="<?php echo base_url('public/assets/css/style.css'); ?>" type="text/css" media="all">
+
+	<!-- GOOGLE FONTS -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800" rel="stylesheet">
+	
+
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+<!-- Preloader -->
+<div class="preloader-outer">
+	<div class="preloader">
+		<div class="lines">
+			<div class="line line-1"></div>
+			<div class="line line-2"></div>
+			<div class="line line-3"></div>
+		</div>
+
+		<div class="loading-text">- Wait -</div>
+	</div>
+</div>
+<!-- Preloader end -->
+
+<!-- Main Wrapper -->
+<main class="wrapper">
+
+	<!-- Header -->
+	<header class="header pull-left">
+
+		<!-- Mobile menu -->
+		<div class="mobile-bar visible-sm visible-xs">
+			<div class="hamburger-menu">
+				  <div class="bar"></div>	
+			</div>
+		</div>
+		<!-- Mobile menu end -->
+
+		<div class="avatar">
+			<img src="<?php echo base_url('public/assets/images/my-pic-1.jpg'); ?>" alt="avatar">
+		</div>
+
+		<div class="name">
+			<h1>Samsu Dhuha</h1>
+			<span>Web Developer</span>
+		</div>
+
+		<!-- Social Icons -->
+		<div class="social-icons">
+			<ul>
+				<!-- <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-flickr" aria-hidden="true"></i></a></li> -->
+			</ul>
+		</div>
+		<!-- Social Icons end -->
+
+		<!-- Navigation bar -->
+		<nav class="main-nav">
+			<ul class="navigation">	
+				<li><a href="#about">About Me</a></li>
+				<li><a href="#education">Education</a></li>
+				<!-- <li><a href="#experience">Expereince</a></li> -->
+			</ul>
+		</nav>
+		<!-- Navigation bar end -->
+
+		<!-- Footer Copyright -->
+		<div class="copyright">
+			<!-- <span>© Copyright 2018 Designstub</span> -->
+		</div>
+		<!-- Footer Copyright end -->
+
+	</header>
+	<!-- Header end -->
+
+	<!-- Main Content -->
+	<div class="main-content pull-right">
+
+		
+		<!-- Section About -->
+		<section id="about" class="about">
+
+			<div class="section-header">
+				<h2>About Me</h2>
+			</div>
+
+			<!-- Intro -->
+			<div class="intro">
+				
+				<p>Hello, My name is Muhamat Samsu Dhuha.</p>
+
+				<ul class="info">
+					<li><strong>Birthday:</strong> 18 April 2021</li>
+					<li><strong>Location:</strong> Samarinda, Kalimantan Timur</li>
+					<li><strong>Email:</strong> msamsudhuha9@gmail.com</li>
+					<li><strong>Phone:</strong> +62 851 5611 4884</li>
+				</ul>
+
+				
+
+			</div>
+			<!-- Intro end -->
+
+			<!-- Skills -->
+			<div class="skills">
+
+				<div class="row">
+
+					<div class="col-md-6 col-sm-6 col-xs-12 item">
+						<div class="skill-info clearfix">
+							<h3 class="pull-left">PHP</h3>
+							<span class="pull-right">90%</span>
+						</div>
+						<div class="progress">
+							<div class="progress-bar" role="progressbar" aria-valuenow="90"
+							aria-valuemin="0" aria-valuemax="100" style="width:90%">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6 col-sm-6 col-xs-12 item">
+						<div class="skill-info clearfix">
+							<h3 class="pull-left">Javascript</h3>
+							<span class="pull-right">80%</span>
+						</div>
+						<div class="progress">
+							<div class="progress-bar" role="progressbar" aria-valuenow="80"
+							aria-valuemin="0" aria-valuemax="100" style="width:80%">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6 col-sm-6 col-xs-12 item">
+						<div class="skill-info clearfix">
+							<h3 class="pull-left">Python</h3>
+							<span class="pull-right">65%</span>
+						</div>
+						<div class="progress">
+							<div class="progress-bar" role="progressbar" aria-valuenow="65"
+							aria-valuemin="0" aria-valuemax="100" style="width:65%">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6 col-sm-6 col-xs-12 item">
+						<div class="skill-info clearfix">
+							<h3 class="pull-left">SQL</h3>
+							<span class="pull-right">95%</span>
+						</div>
+						<div class="progress">
+							<div class="progress-bar" role="progressbar" aria-valuenow="95"
+							aria-valuemin="0" aria-valuemax="100" style="width:95%">
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+			<!-- Skills end -->
+		</section>
+		<!-- Section About end -->
+
+		
+
+		<!-- Section Resume -->
+		<section id="experience" class="resume">
+			
+			<div class="section-header">
+				<h2>Education</h2>
+				
+			</div>
+
+			<div class="row">
+
+				
+				
+						<div class="col-md-12 col-sm-12 col-xs-12">
+							<div class="top-item resume-item">
+								<h2>Sekolah Dasar</h2>
+								<span>SD Islam Bunga Bangsa | 2016 - 2012</span>
+								<p>-</p>
+							</div>
+						</div>
+						
+						<div class="col-md-12 col-sm-12 col-xs-12">
+							<div class="resume-item">
+								<h2>Sekolah Menengah Pertama</h2>
+								<span>SMP Islam Bunga Bangsa |  2012 - 2015</span>
+								<p>-</p>
+							</div>
+						</div>
+
+						<div class="col-md-12 col-sm-12 col-xs-12">
+							<div class="resume-item">
+								<h2>Sekolah Menengah Akhir</h2>
+								<span>SMA Negeri 1 Samarinda |  2015 - 2018</span>
+								<p>Jurusan IPA</p>
+							</div>
+						</div>
+
+
+						<div class="col-md-12 col-sm-12 col-xs-12">
+							<div class="resume-item">
+								<h2>Perguruan Tinggi</h2>
+								<span>Institut Teknologi Sepuluh Nopember  |  2018 - Sekarang</span>
+								<p>Jurusan Informatika</p>
+							</div>
+						</div>
+
+					<!-- Page 1 End -->
+
+				
+	
+			</div>
+
+		</section>
+		<!-- Section Resume end -->
+
+		<!-- Section Works -->
+		<!-- <section id="works" class="works clearfix">
+			
+			<div class="section-header">
+				<h2>Portfolio</h2>
+			</div>
+
+			<div class="control">
+				<ul>
+					<li class="active"><a class="filter" data-filter="all">All Projects</a></li>
+					<li><a class="filter" data-filter="1">Logo</a></li>
+					<li><a class="filter" data-filter="2">Vector</a></li>
+					<li><a class="filter" data-filter="3">Audio</a></li>
+					<li><a class="filter" data-filter="4">Video</a></li>
+				</ul>
+			</div>
+
+			<div class="item-outer row clearfix">
+				<div class="col-md-4 col-sm-6 col-xs-12 filtr-item" data-category="1" data-sort="value">
+					<div class="item">
+						<a href="<?php echo base_url('public/assets/images/work1.jpg'); ?>" class="work-image">
+							<div class="title">
+								<div class="inner">
+									<h2>Project Name</h2>
+									<span>View Details</span>
+								</div>
+							</div>
+						</a>
+						<div class="overlay"></div>
+						<img src="<?php echo base_url('public/assets/images/work1.jpg'); ?>" alt="portfolio">
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-6 col-xs-12 filtr-item" data-category="2" data-sort="value">
+					<div class="item">
+						<a href="<?php echo base_url('public/assets/images/work2.jpg'); ?>" class="work-image">
+							<div class="title">
+								<div class="inner">
+									<h2>Project Name</h2>
+									<span>View Details</span>
+								</div>
+							</div>
+						</a>
+						<div class="overlay"></div>
+						<img src="<?php echo base_url('public/assets/images/work2.jpg'); ?>" alt="portfolio">
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-6 col-xs-12 filtr-item" data-category="1" data-sort="value">
+					<div class="item">
+						<a href="<?php echo base_url('public/assets/images/work3.jpg'); ?>" class="work-image">
+							<div class="title">
+								<div class="inner">
+									<h2>Project Name</h2>
+									<span>View Details</span>
+								</div>
+							</div>
+						</a>
+						<div class="overlay"></div>
+						<img src="<?php echo base_url('public/assets/images/work3.jpg'); ?>" alt="portfolio">
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-6 col-xs-12 filtr-item" data-category="2" data-sort="value">
+					<div class="item">
+						<a href="<?php echo base_url('public/assets/images/work4.jpg'); ?>" class="work-image">
+							<div class="title">
+								<div class="inner">
+									<h2>Project Name</h2>
+									<span>View Details</span>
+								</div>
+							</div>
+						</a>
+						<div class="overlay"></div>
+						<img src="<?php echo base_url('public/assets/images/work4.jpg'); ?>" alt="portfolio">
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-6 col-xs-12 filtr-item" data-category="3" data-sort="value">
+					<div class="item">
+						<a href="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/237603952&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true" class="work-video">
+							<div class="title">
+								<div class="inner">
+									<h2>Project Name</h2>
+									<span>View Details</span>
+								</div>
+							</div>
+						</a>
+						<div class="overlay"></div>
+						<img src="<?php echo base_url('public/assets/images/work5.jpg'); ?>" alt="portfolio">
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-6 col-xs-12 filtr-item" data-category="4" data-sort="value">
+					<div class="item">
+						<a href="https://www.youtube.com/watch?v=fZdkJKFwDE0" class="work-video">
+							<div class="title">
+								<div class="inner">
+									<h2>Project Name</h2>
+									<span>View Details</span>
+								</div>
+							</div>
+						</a>
+						<div class="overlay"></div>
+						<img src="<?php echo base_url('public/assets/images/work6.jpg'); ?>" alt="portfolio">
+					</div>
+				</div>
+			</div>
+			
+		</section> -->
+		<!-- Section Works end -->
+
+	</div>
+	<!-- Main Content end -->
+
+</main>
+<!-- Main Wrapper end -->
+
+<!-- SCRIPTS -->
+<script type="text/javascript" src="<?php echo base_url('public/assets/js/jquery-1.12.3.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/assets/js/jquery.onepage-scroll.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/assets/js/jquery.easing.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/assets/js/jquery.backstretch.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/assets/js/jquery.filterizr.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/assets/js/jquery.magnific-popup.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/assets/js/owl.carousel.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/assets/js/custom.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('public/assets/js/smoothscroll.min.js'); ?>"></script>
+
+</body>
+</html>
